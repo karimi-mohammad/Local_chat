0f8ab1525fe6d1c944681bedd049c6becba4c2d2
Mohammad Karimi <zed.light.tube@gmail.com>
2024-01-02 21:05:35 +0330

    Fix : Send Func is completed

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -5,8 +5,33 @@ class chat {
     send() {
         var msgText = document.querySelector("#Message").value;
         var userName = Cookies.get('userName');
+        console.log(userName);
+        if (userName && userName != "") {
+            if (msgText && msgText != "") {
+                const data = {
+                    "userName": userName,
+                    "Msg": msgText
+                }
+                const requestOption = {
+                    method: 'POST', headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(data)
+                }
+                fetch("./api/sendMsg.php", requestOption)
+                    .then(response => {
+                        console.log(response);
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    })
+            } else {
+                alert("msg text in empty")
+            }
+        } else {
+            alert("at first you should set a user name")
+        }
 
-        console.log(`saas`);
     }
     setName() {
         var userName = document.querySelector("#username").value
